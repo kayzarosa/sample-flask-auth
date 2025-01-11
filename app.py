98388df9049adea6b3b48b9b7cb5eeecdff1dcf3
kayza <kayzakizy@gmail.com>
2025-01-11 19:14:44 -0300

    add bcrypt no codigo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import bcrypt
 from models.user import User
 from database import db
 from flask_login import (
@@ -34,7 +35,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             return jsonify({"message": "Autenticação realizada com sucesso"})
 
@@ -55,7 +56,8 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password, role="user")
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user")
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
@@ -83,7 +85,9 @@ def update_user(id_user):
         return jsonify({"message": "Operação não permitida"}), 403
 
     if user and data.get("password"):
-        user.password = data.get("password")
+        user.password = bcrypt.hashpw(
+            str.encode(data.get("password")), bcrypt.gensalt()
+        )
         db.session.commit()
 
         return jsonify({"message": f"Usuário {id_user} atualizado com sucesso!"})
